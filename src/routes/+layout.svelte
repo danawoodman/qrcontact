a05a19a141619fac9b4f2190036063caf9f424ea
Dana Woodman <dana@danawoodman.com>
2022-08-21 19:09:00 -0700

    Page transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,20 @@
 	import "../app.postcss";
 	import Footer from "$lib/components/Footer.svelte";
 	import Header from "$lib/components/Header.svelte";
+	import { navigating } from "$app/stores";
+	import { fade } from "svelte/transition";
+
+	let duration = 300;
+
+	$: changed = $navigating?.from !== $navigating?.to;
 </script>
 
 <Header />
 
-<slot />
+{#key changed}
+	<div in:fade={{ delay: duration, duration }} out:fade={{ duration }}>
+		<slot />
+	</div>
+{/key}
 
 <Footer />
--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -56,281 +56,294 @@
 	}
 </script>
 
-<main class="md:flex items-start mx-auto justify-center px-8">
-	<section class="max-w-prose sm:mr-12">
-		<!-- <section id="photo" class="mt-20">
+<main>
+	<div class="md:flex items-start mx-auto justify-center px-8">
+		<div class="max-w-prose sm:mr-12">
+			<!-- <section id="photo" class="mt-20">
 		<PhotoUploader />
 		</section> -->
 
-		<section id="name" class="my-12">
-			<h3 class="section-heading">Your name</h3>
-			<fieldset class="flex items-center">
-				<input
-					type="text"
-					class="input w-full"
-					bind:value={$contact.first_name}
-				/>
-				<input
-					type="text"
-					class="input mx-2 w-20 sm:w-28"
-					placeholder="Middle name..."
-					bind:value={$contact.middle_name}
-				/>
-				<input
-					type="text"
-					class="input w-full"
-					placeholder="Last name..."
-					bind:value={$contact.last_name}
-				/>
-			</fieldset>
-		</section>
+			<section id="name" class="my-12">
+				<h3 class="section-heading">Your name</h3>
+				<fieldset class="flex items-center">
+					<input
+						type="text"
+						class="input w-full"
+						bind:value={$contact.first_name}
+					/>
+					<input
+						type="text"
+						class="input mx-2 w-20 sm:w-28"
+						placeholder="Middle name..."
+						bind:value={$contact.middle_name}
+					/>
+					<input
+						type="text"
+						class="input w-full"
+						placeholder="Last name..."
+						bind:value={$contact.last_name}
+					/>
+				</fieldset>
+			</section>
 
-		<section id="phone-numbers" class="my-12">
-			<h3 class="section-heading">Phone numbers</h3>
-			{#if $contact?.phone_numbers?.length}
-				{#each $contact.phone_numbers as phone, i}
-					<fieldset
-						class="my-2 flex items-center"
-						transition:slide={{ duration: 200 }}
-					>
-						<input
-							type="text"
-							placeholder="Label..."
-							class="input w-24 sm:w-36"
-							bind:value={phone.label}
-						/>
-						<input
-							type="text"
-							placeholder="Phone number..."
-							class="input ml-2 w-full flex-1"
-							bind:value={phone.number}
-						/>
-						<button
-							class="btn --sm --icon ml-2"
-							on:click={() => remove_phone(i)}
+			<section id="phone-numbers" class="my-12">
+				<h3 class="section-heading">Phone numbers</h3>
+				{#if $contact?.phone_numbers?.length}
+					{#each $contact.phone_numbers as phone, i}
+						<fieldset
+							class="my-2 flex items-center"
+							transition:slide={{ duration: 200 }}
 						>
-							<CloseIcon />
-						</button>
-					</fieldset>
-				{/each}
-			{/if}
-			<p class="mt-4">
-				<button
-					class="link --sm"
-					on:click={() => {
-						$contact.phone_numbers = [
-							...($contact.phone_numbers ?? []),
-							{ label: "", number: "" },
-						];
-					}}
-				>
-					Add phone number
-				</button>
-			</p>
-		</section>
-
-		<section id="emails" class="my-12">
-			<h3 class="section-heading">Email addresses</h3>
-			{#if $contact?.emails?.length}
-				{#each $contact.emails as email, i}
-					<fieldset
-						class="my-2 flex items-center"
-						transition:slide={{ duration: 200 }}
-					>
-						<input
-							type="text"
-							placeholder="Label..."
-							class="input w-24 sm:w-36"
-							bind:value={email.label}
-						/>
-						<input
-							type="text"
-							placeholder="Email address..."
-							class="input ml-2 w-full flex-1"
-							bind:value={email.address}
-						/>
-						<button
-							class="btn --sm --icon ml-2"
-							on:click={() => remove_email(i)}
-						>
-							<CloseIcon />
-						</button>
-					</fieldset>
-				{/each}
-			{/if}
-			<p class="mt-4">
-				<button
-					class="link --sm"
-					on:click={() => {
-						$contact.emails = [
-							...($contact.emails ?? []),
-							{ label: "", address: "" },
-						];
-					}}
-				>
-					Add email address
-				</button>
-			</p>
-		</section>
-
-		<section id="urls" class="my-12">
-			<h3 class="section-heading">URLs</h3>
-			{#if $contact?.urls?.length}
-				{#each $contact.urls as email, i}
-					<fieldset
-						class="my-2 flex items-center"
-						transition:slide={{ duration: 200 }}
-					>
-						<input
-							type="text"
-							placeholder="Label..."
-							class="input w-24 sm:w-36"
-							bind:value={email.label}
-						/>
-						<input
-							type="text"
-							placeholder="Email address..."
-							class="input ml-2 w-full flex-1"
-							bind:value={email.href}
-						/>
-						<button class="btn --sm --icon ml-2" on:click={() => remove_url(i)}>
-							<CloseIcon />
-						</button>
-					</fieldset>
-				{/each}
-			{/if}
-			<p class="mt-4">
-				<button
-					class="link --sm"
-					on:click={() => {
-						$contact.urls = [...($contact.urls ?? []), { label: "", href: "" }];
-					}}
-				>
-					Add URL
-				</button>
-			</p>
-		</section>
-
-		<section id="addresses" class="my-12">
-			<h3 class="section-heading">Addresses</h3>
-			{#if $contact?.addresses?.length}
-				{#each $contact.addresses as address, i}
-					<fieldset
-						class="my-4 sm:flex items-start"
-						transition:slide={{ duration: 200 }}
-					>
-						<div class="sm:w-36">
 							<input
 								type="text"
 								placeholder="Label..."
-								class="input --block"
-								bind:value={address.label}
+								class="input w-24 sm:w-36"
+								bind:value={phone.label}
 							/>
-						</div>
-						<div class="flex-1 sm:ml-2">
-							<div class="flex flex-col sm:flex-row items-center">
-								<input
-									type="text"
-									placeholder="Street address..."
-									class="input w-full mt-2 sm:mr-2 sm:mt-0"
-									bind:value={address.street}
-								/>
-								<input
-									type="text"
-									placeholder="Street address 2..."
-									class="input w-full mt-2 sm:mt-0"
-									bind:value={address.street_2}
-								/>
-							</div>
-							<div class="flex items-center mt-2">
-								<input
-									type="text"
-									placeholder="City..."
-									class="input w-full mr-2"
-									bind:value={address.city}
-								/>
-								<input
-									type="text"
-									placeholder="State..."
-									class="input w-full"
-									bind:value={address.state_province}
-								/>
-							</div>
-							<div class="flex items-center mt-2">
-								<input
-									type="text"
-									placeholder="Postal code..."
-									class="input w-full mr-2"
-									bind:value={address.postal_code}
-								/>
-								<input
-									type="text"
-									placeholder="Country..."
-									class="input w-full"
-									bind:value={address.country_region}
-								/>
-							</div>
-						</div>
-						<button
-							class="btn --sm --icon mt-2 sm:mt-0 sm:ml-2"
-							on:click={() => remove_address(i)}
+							<input
+								type="text"
+								placeholder="Phone number..."
+								class="input ml-2 w-full flex-1"
+								bind:value={phone.number}
+							/>
+							<button
+								class="btn --sm --icon ml-2"
+								on:click={() => remove_phone(i)}
+							>
+								<CloseIcon />
+							</button>
+						</fieldset>
+					{/each}
+				{/if}
+				<p class="mt-4">
+					<button
+						class="link --sm"
+						on:click={() => {
+							$contact.phone_numbers = [
+								...($contact.phone_numbers ?? []),
+								{ label: "", number: "" },
+							];
+						}}
+					>
+						Add phone number
+					</button>
+				</p>
+			</section>
+
+			<section id="emails" class="my-12">
+				<h3 class="section-heading">Email addresses</h3>
+				{#if $contact?.emails?.length}
+					{#each $contact.emails as email, i}
+						<fieldset
+							class="my-2 flex items-center"
+							transition:slide={{ duration: 200 }}
 						>
-							<CloseIcon />
-						</button>
-					</fieldset>
-				{/each}
-			{/if}
-			<button
-				class="link --sm"
-				on:click={() => {
-					$contact.addresses = [
-						...($contact.addresses ?? []),
-						{ label: "", street: "" },
-					];
-				}}
-			>
-				Add address
-			</button>
+							<input
+								type="text"
+								placeholder="Label..."
+								class="input w-24 sm:w-36"
+								bind:value={email.label}
+							/>
+							<input
+								type="text"
+								placeholder="Email address..."
+								class="input ml-2 w-full flex-1"
+								bind:value={email.address}
+							/>
+							<button
+								class="btn --sm --icon ml-2"
+								on:click={() => remove_email(i)}
+							>
+								<CloseIcon />
+							</button>
+						</fieldset>
+					{/each}
+				{/if}
+				<p class="mt-4">
+					<button
+						class="link --sm"
+						on:click={() => {
+							$contact.emails = [
+								...($contact.emails ?? []),
+								{ label: "", address: "" },
+							];
+						}}
+					>
+						Add email address
+					</button>
+				</p>
+			</section>
+
+			<section id="urls" class="my-12">
+				<h3 class="section-heading">URLs</h3>
+				{#if $contact?.urls?.length}
+					{#each $contact.urls as email, i}
+						<fieldset
+							class="my-2 flex items-center"
+							transition:slide={{ duration: 200 }}
+						>
+							<input
+								type="text"
+								placeholder="Label..."
+								class="input w-24 sm:w-36"
+								bind:value={email.label}
+							/>
+							<input
+								type="text"
+								placeholder="Email address..."
+								class="input ml-2 w-full flex-1"
+								bind:value={email.href}
+							/>
+							<button
+								class="btn --sm --icon ml-2"
+								on:click={() => remove_url(i)}
+							>
+								<CloseIcon />
+							</button>
+						</fieldset>
+					{/each}
+				{/if}
+				<p class="mt-4">
+					<button
+						class="link --sm"
+						on:click={() => {
+							$contact.urls = [
+								...($contact.urls ?? []),
+								{ label: "", href: "" },
+							];
+						}}
+					>
+						Add URL
+					</button>
+				</p>
+			</section>
+
+			<section id="addresses" class="my-12">
+				<h3 class="section-heading">Addresses</h3>
+				{#if $contact?.addresses?.length}
+					{#each $contact.addresses as address, i}
+						<fieldset
+							class="my-4 sm:flex items-start"
+							transition:slide={{ duration: 200 }}
+						>
+							<div class="sm:w-36">
+								<input
+									type="text"
+									placeholder="Label..."
+									class="input --block"
+									bind:value={address.label}
+								/>
+							</div>
+							<div class="flex-1 sm:ml-2">
+								<div class="flex flex-col sm:flex-row items-center">
+									<input
+										type="text"
+										placeholder="Street address..."
+										class="input w-full mt-2 sm:mr-2 sm:mt-0"
+										bind:value={address.street}
+									/>
+									<input
+										type="text"
+										placeholder="Street address 2..."
+										class="input w-full mt-2 sm:mt-0"
+										bind:value={address.street_2}
+									/>
+								</div>
+								<div class="flex items-center mt-2">
+									<input
+										type="text"
+										placeholder="City..."
+										class="input w-full mr-2"
+										bind:value={address.city}
+									/>
+									<input
+										type="text"
+										placeholder="State..."
+										class="input w-full"
+										bind:value={address.state_province}
+									/>
+								</div>
+								<div class="flex items-center mt-2">
+									<input
+										type="text"
+										placeholder="Postal code..."
+										class="input w-full mr-2"
+										bind:value={address.postal_code}
+									/>
+									<input
+										type="text"
+										placeholder="Country..."
+										class="input w-full"
+										bind:value={address.country_region}
+									/>
+								</div>
+							</div>
+							<button
+								class="btn --sm --icon mt-2 sm:mt-0 sm:ml-2"
+								on:click={() => remove_address(i)}
+							>
+								<CloseIcon />
+							</button>
+						</fieldset>
+					{/each}
+				{/if}
+				<button
+					class="link --sm"
+					on:click={() => {
+						$contact.addresses = [
+							...($contact.addresses ?? []),
+							{ label: "", street: "" },
+						];
+					}}
+				>
+					Add address
+				</button>
+			</section>
+		</div>
+
+		<section
+			id="qrcode"
+			class="my-12 flex flex-col items-center justify-center w-64"
+		>
+			<div class="bg-white shadow-md rounded-lg p-4">
+				<QrCode data={$vcard} bind:qrcode />
+			</div>
+			<p class="text-center mt-12">
+				<a href={qrcode} download="{$file_name}.png" class="btn">Download</a>
+			</p>
 		</section>
-	</section>
+	</div>
 
-	<section
-		id="qrcode"
-		class="my-12 flex flex-col items-center justify-center w-64"
-	>
-		<div class="bg-white shadow-md rounded-lg p-4">
-			<QrCode data={$vcard} bind:qrcode />
-		</div>
-		<p class="text-center mt-12">
-			<a href={qrcode} download="{$file_name}.png" class="btn">Download</a>
-		</p>
-	</section>
-</main>
+	<div class="mx-auto max-w-prose">
+		<section id="vcard-data">
+			<p class="mt-20 mb-4 text-center">
+				<button class="link --sm" on:click={() => (view_vcard = !view_vcard)}>
+					{#if view_vcard}
+						Hide
+					{:else}
+						View raw vCard data
+					{/if}
+				</button>
+			</p>
 
-<section id="vcard-data">
-	<p class="mt-20 mb-4 text-center">
-		<button class="link --sm" on:click={() => (view_vcard = !view_vcard)}>
 			{#if view_vcard}
-				Hide
-			{:else}
-				View raw vCard data
+				<div transition:slide>
+					<pre class="p-6 bg-slate-200 rounded-md overflow-auto">{$vcard}</pre>
+				</div>
 			{/if}
-		</button>
-	</p>
+		</section>
 
-	{#if view_vcard}
-		<div transition:slide>
-			<pre class="p-6 bg-slate-200 rounded-md overflow-auto">{$vcard}</pre>
-		</div>
-	{/if}
-</section>
-
-<section id="security-note" class="text-sm text-slate-500 mt-12 text-center">
-	<p class="mb-2">
-		🔐 <strong>Security note:</strong>
-	</p>
-	<p>Nothing you input on this page leaves this page, your data is safe!</p>
-</section>
+		<section
+			id="security-note"
+			class="text-sm text-slate-500 mt-12 text-center"
+		>
+			<p class="mb-2">
+				🔐 <strong>Security note:</strong>
+			</p>
+			<p>Nothing you input on this page leaves this page, your data is safe!</p>
+		</section>
+	</div>
+</main>
 
 <style lang="postcss">
 	.section-heading {
