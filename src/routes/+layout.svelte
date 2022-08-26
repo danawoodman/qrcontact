7c7ed52e0368c81367852d82925da45b9a4f15db
Dana Woodman <dana@danawoodman.com>
2022-08-25 17:48:54 -0700

    Add page title/desc to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,14 @@
 	$: changed = $navigating?.from !== $navigating?.to;
 </script>
 
+<svelte:head>
+	<title>qrcontact - QR code contact generator by Dana Woodman</title>
+	<meta
+		name="description"
+		content="Generate a QR code for your contact information, share it with friends and customers."
+	/>
+</svelte:head>
+
 <Header />
 
 {#key changed}
