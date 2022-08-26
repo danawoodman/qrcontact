5eb27a51f807663f57782dcc62108cdaffdb0b05
Dana Woodman <dana@danawoodman.com>
2022-08-25 17:37:10 -0700

    Add icon to header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,12 @@
+<script>
+	import QrCodeIcon from "./QRCodeIcon.svelte";
+</script>
+
 <header class="my-12 mx-auto max-w-prose text-center">
 	<h1 id="logo" class="">
-		<a href="/" class="btn --sm"> qrcontact </a>
+		<a href="/" class="btn --sm">
+			<QrCodeIcon class="inline-block text-black mr-2" />
+			qrcontact
+		</a>
 	</h1>
 </header>
